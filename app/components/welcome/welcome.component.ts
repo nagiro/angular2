c6d13b04dbef21f5986f6c4080fbe8f931fb2ea9
rahil471 <rahilsk47@gmail.com>
2016-03-10 19:37:39 +0530

    first commit

---
+++ app/components/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '../app/components/welcome/welcome.template.html'
+})
+export class WelcomeComponent {
+    constructor() {
+        var self = this;
+        self.displayName = '';
+    }
+    setDisplayName(value){
+        var self = this;
+        self.displayName = value;
+    }
+}
